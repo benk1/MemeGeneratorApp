60b8cdca13b45b4b5ef8ebebf57bdfa7cbd6fc4e
benk1 <kakengi_bernard@hotmail.com>
2020-02-10 08:46:30 +0200

    Updates

---
+++ precache-manifest.c145c425a2027b1d11ead616d79ef239.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a59783181e6b3023e6ca56d33ec54b50",
+    "url": "/meme-app/index.html"
+  },
+  {
+    "revision": "23830d8a8cc1503d85b3",
+    "url": "/meme-app/static/css/main.418f04ae.chunk.css"
+  },
+  {
+    "revision": "02d3562d5ddbb586b790",
+    "url": "/meme-app/static/js/2.6fa09ccd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/meme-app/static/js/2.6fa09ccd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "23830d8a8cc1503d85b3",
+    "url": "/meme-app/static/js/main.5d9c17d7.chunk.js"
+  },
+  {
+    "revision": "8c03f2f614a253dbf4c7",
+    "url": "/meme-app/static/js/runtime-main.0829aa9f.js"
+  }
+]);
